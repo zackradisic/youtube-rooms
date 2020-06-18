c1b8a43f3692f866283ca742eaa687e761c1b194
zackradisic <zack@theradisic.com>
2020-06-18 11:38:05 -0400

    add name field for room

--- internal/models/room.go
+++ internal/models/room.go
@@ -7,4 +7,5 @@ type Room struct {
 	gorm.Model
 	Owner          uint   `gorm:"foreignkey:rooms_owner_fk_users_id;UNIQUE_INDEX;NOT_NULL"`
 	HashedPassword string `gorm:"type:CHAR(76)"`
+	Name           string `gorm:"type:VARCHAR(36);UNIQUE_INDEX;NOT_NULL"`
 }
