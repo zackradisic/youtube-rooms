51fb2c9a05fbdf7525e974e014d0baa017de3f9e
zackradisic <zack@theradisic.com>
2020-06-18 04:37:37 -0400

    made foreign key uniquely indexed and not null

--- internal/models/user_auth.go
+++ internal/models/user_auth.go
@@ -8,7 +8,7 @@ type UserAuth struct {
 	AccessToken  string `gorm:"type:VARCHAR(30);index"`
 	RefreshToken string `gorm:"type:VARCHAR(30);index"`
 	ExpiresIn    uint   `gorm:"type:UNSIGNED INT(10);index"`
-	User         uint   `gorm:"foreignkey:user_auth_user_fk_user_id"`
+	User         uint   `gorm:"foreignkey:user_auth_user_fk_user_id;UNIQUE_INDEX;NOT_NULL"`
 }
 
 // TableName sets the UserAuth model's table name as user_auth
