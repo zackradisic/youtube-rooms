cbbcee79ab96714f3e4180f6657ebd968dbce485
zackradisic <zack@theradisic.com>
2020-06-18 04:46:34 -0400

    change foreign key name

--- internal/models/room.go
+++ internal/models/room.go
@@ -5,6 +5,6 @@ import "github.com/jinzhu/gorm"
 // Room is a room a user can create, it can optionally be secured with a passowrd
 type Room struct {
 	gorm.Model
-	Owner          uint   `gorm:"foreignkey:user_auth_user_fk_user_id;UNIQUE_INDEX;NOT_NULL"`
+	Owner          uint   `gorm:"foreignkey:rooms_owner_fk_user_id;UNIQUE_INDEX;NOT_NULL"`
 	HashedPassword string `gorm:"type:CHAR(76)"`
 }
