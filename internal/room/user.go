47719a7f1af94e41bcda61c2150026ecf1b781f7
zackradisic <zack@theradisic.com>
2020-06-26 10:56:55 -0400

    add function to get discord handle

--- internal/room/user.go
+++ internal/room/user.go
@@ -1,6 +1,10 @@
 package room
 
-import "github.com/zackradisic/youtube-rooms/internal/models"
+import (
+	"fmt"
+
+	"github.com/zackradisic/youtube-rooms/internal/models"
+)
 
 // User represents a user in the room
 type User struct {
@@ -15,4 +19,10 @@ func NewUser(user *models.User, userAuth *models.UserAuth) *User {
 	newUser.Model = user
 	newUser.UserAuth = userAuth
 	return newUser
+}
+
+// DiscordHandle returns the user's username and discriminator as it appears on discord
+// Ex: jeremy#5932
+func (u *User) DiscordHandle() string {
+	return fmt.Sprintf("%s#%s", u.Model.LastDiscordUsername, u.Model.LastDiscordDiscriminator)
 }
