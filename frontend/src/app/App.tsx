6429628982b1d17761e6610cb4c488deb91496ce
zackradisic <zack@theradisic.com>
2020-07-07 18:31:35 -0400

    install react-router-dom

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 import WebSocketProvider, { WebSocketContext } from '../websocket/context'
 import Player from '../features/player/Player'
+
+import logo from './logo.svg'
+import './App.css'
 
 const App = () => {
   return (
